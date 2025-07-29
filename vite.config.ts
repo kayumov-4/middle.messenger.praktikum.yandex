54c7de43f0d7820878b645e7a7a963e4c35d1692
kayumov-4 <maksud8489@gmail.com>
2025-07-29 22:51:44 +0500

    Chats page and 404page added

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from "vite";
 import vitePluginString from "vite-plugin-string";
+import { fileURLToPath } from "node:url";
+import { dirname, resolve } from "node:path";
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+      },
+    },
+  },
   plugins: [
     vitePluginString({
       include: ["**/*.hbs"],
