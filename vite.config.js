9d86bdcaa0462130bad3b5a321c9288e3ea7fa5d
kayumov-4 <maksud8489@gmail.com>
2025-08-02 14:27:09 +0500

    laaast

--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,27 @@
 import { defineConfig } from "vite";
 import vitePluginString from "vite-plugin-string";
-import { fileURLToPath } from "node:url";
-import { dirname, resolve } from "node:path";
-const __dirname = dirname(fileURLToPath(import.meta.url));
+import { resolve } from "node:path";
+import history from "connect-history-api-fallback";
 
 export default defineConfig({
   server: {
     port: 3000,
-    strictPort: true,
-  },
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "index.html"),
-      },
-    },
+    middlewareMode: true,
   },
   plugins: [
+    {
+      name: "spa-fallback",
+      configureServer(server) {
+        server.middlewares.use(history());
+      },
+    },
     vitePluginString({
       include: ["**/*.hbs"],
     }),
   ],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@import "/src/style.css";`,
-      },
+  build: {
+    rollupOptions: {
+      input: resolve(__dirname, "index.html"),
     },
   },
 });
