5a8ec587d63083a9ba8eeb7298ee8ccad0c3b001
kayumov-4 <maksud8489@gmail.com>
2025-08-02 14:24:10 +0500

    sprint 1

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,20 @@
 import { defineConfig } from "vite";
 import vitePluginString from "vite-plugin-string";
-import { fileURLToPath } from "url";
-import { dirname, resolve } from "path";
-
+import { fileURLToPath } from "node:url";
+import { dirname, resolve } from "node:path";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   server: {
     port: 3000,
+    strictPort: true,
   },
   build: {
     rollupOptions: {
-      input: resolve(__dirname, "index.html"),
+      input: {
+        main: resolve(__dirname, "index.html"),
+      },
     },
-    outDir: "dist",
   },
   plugins: [
     vitePluginString({
