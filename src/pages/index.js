3c341e6e20af18850fd9925f3f2317112d3bf01b
kayumov-4 <maksud8489@gmail.com>
2025-08-03 16:26:18 +0500

    deploy mode

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import LoginPage from "./loginPage.hbs";
+import RegistrationPage from "./registerPage.hbs";
+import NotFoundPage from "./404.hbs";
+import ErrorPage from "./500.hbs";
+import ChatPage from "./chatPage.hbs";
+import NavigationPage from "./navigationPage.hbs";
+
+export {
+  LoginPage,
+  RegistrationPage,
+  NotFoundPage,
+  ErrorPage,
+  ChatPage,
+  NavigationPage,
+};
