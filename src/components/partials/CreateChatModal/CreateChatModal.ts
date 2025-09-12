fc20c419cca5d84a5a7aa3e368f1d3eff189b8e2
kayumov-4 <maksud8489@gmail.com>
2025-09-12 19:48:01 +0500

    Sprint 3

--- src/components/partials/CreateChatModal/CreateChatModal.ts
+++ src/components/partials/CreateChatModal/CreateChatModal.ts
@@ -2,7 +2,7 @@ import Block from "../../../../core/Block";
 import Button from "../../ui/Button/Button";
 import Input from "../../ui/Input/Input";
 import template from "./CreateChatModal.hbs";
-import "./createChatModal.scss";
+import "./CreateChatModal.scss";
 
 interface CreateChatModalProps {
   onSave: (title: string) => void;
